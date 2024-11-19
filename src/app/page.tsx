e363aa7f47aac0a50138b6eb30723e414f719d3c
Marcus Rådell <marcus@radell.net>
2024-11-19 09:20:48 +0100

    change  div into main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ const data = { total: 10, sick: 5 };
 
 export default function Home() {
   return (
-    <>
+    <main>
       <h1>Sick report</h1>
-      <div>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
-    </>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </main>
   );
 }
